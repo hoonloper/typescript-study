cc54eaf51953a2b75055c16042dadf7934b38c68
YH <yonghoonj98@gmail.com>
2022-10-11 10:52:04 +0900

    Add: 함수 타입 Call Signatures 예제 추가

--- function-type.ts
+++ function-type.ts
@@ -26,3 +26,26 @@ newPlayer.age = 20;
 const secondPlayerMaker = (name: string): Player => ({
   name,
 });
+
+/*
+Call Signatures
+- 함수 혹은 변수에 마우스를 올렸을 때 추론하는 타입을 칭함.
+- 함수의 동작을 타입으로 지정할 수 있음.
+*/
+const beforeCallSignatures = (num1: number, num2: number) => num1 + num2;
+
+type CallSignatures = (num1: number, num2: number) => number;
+const afterCallSignatures: CallSignatures = (num1, num2) => num1 + num2;
+const errorCallSignatures: CallSignatures = (num1, num2) => "" + num1 + num2; // Error
+
+/*
+Polymorphism(다형성)
+*/
+
+/*
+Overloading(오버로딩)
+*/
+
+/*
+Generics(제네릭)
+*/
