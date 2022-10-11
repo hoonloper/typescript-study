f28e5a92737ab61092c0ad26dcdfbe1c6c25aaa0
YH <yonghoonj98@gmail.com>
2022-10-11 10:29:13 +0900

    Add: 기본 타입 any 예제 추가

--- normally-type.ts
+++ normally-type.ts
@@ -5,3 +5,16 @@ const num: number = 1;
 const arrStr: string[] = ["string"];
 const arrBool: boolean[] = [true];
 const arrNum: number[] = [1, 2, 3];
+
+const varUndefined: undefined = undefined;
+const varNull: null = null;
+
+/* 
+any 타입
+any 타입은 TS로부터 자유로워지고 싶을 때 사용, TS 비활성화
+빈값은 any가 됨
+*/
+const a: any[] = [1, 2, 3, 4];
+const b: any = true;
+
+a + b; // 동작
