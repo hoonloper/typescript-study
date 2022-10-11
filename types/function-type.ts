2ecfcea46b50be2ae11656d5035bf2bcf8a91da5
YH <yonghoonj98@gmail.com>
2022-10-11 11:49:37 +0900

    Refactor: 폴더 구조 변경

--- types/function-type.ts
+++ types/function-type.ts
@@ -86,6 +86,7 @@ const overloadingArgs: OverloadingArgs = (a: number, b: number, c?: number) => {
 };
 
 /*
+Polymorphism(다형성)
 Generics(제네릭)
 제네릭은 Placeholder와 같음
 */
@@ -158,7 +159,3 @@ const varGeneric: PlusGenericType = {
     isResult: true,
   },
 };
-
-/*
-Polymorphism(다형성)
-*/
