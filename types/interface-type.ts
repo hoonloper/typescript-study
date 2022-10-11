9c9fb80fabd3e61b765451463de482caacb143c2
YH <yonghoonj98@gmail.com>
2022-10-11 15:05:36 +0900

    Add: Interface, Type 차이점 추가

--- types/interface-type.ts
+++ types/interface-type.ts
@@ -1,3 +1,62 @@
+/*
+Type, Interface 차이점
+Interface
+- 원하는 메소드와 property를 클래스가 가지도록 강제할 수 있음
+- 자바스크립트로 컴파일되지 않음
+- 추상클래스와 비슷한 보호를 제공하지만 자바스크립트 파일에서 보이지 않음
+- 클래스의 특정 모양을 정해주는 경우에 Interface를 사용하는 게 좋음(추상 클래스는 코드가 남기 때문)
+
+Type을 쓰고 싶으면 type 키워드 사용
+- object 모양을 만들거나, 타입 alias를 만듦 또는 특정된 값
+
+보통 오브젝트 모양을 알려주기 위해서 Interface를 사용하고 이 외에는 Type을 사용
+*/
+type TypeA = {
+  type: string;
+};
+
+type TypeAA = TypeA & {
+  type2: number;
+};
+
+const typeA: TypeAA = {
+  type: "string",
+  type2: 1,
+};
+
+interface TypeB {
+  type: string;
+}
+
+interface TypeBB extends TypeB {
+  type2: number;
+}
+
+// 새로운 프로퍼티를 생성할 때
+interface TypeBB {
+  type3: boolean;
+}
+
+const typeB: TypeBB = {
+  type: "string",
+  type2: 1,
+  type3: false,
+};
+
+// 추상 클래스 대체 가능
+type TypeX = {
+  type: string;
+};
+
+interface TypeY {
+  type: string;
+}
+
+class TypeClass implements TypeX, TypeY {
+  constructor(public type: string) {}
+}
+
+/* ================================ */
 type StringType = string;
 type NumberType = 1 | 5 | 10;
 type StringsType = "str" | "string" | "s";
